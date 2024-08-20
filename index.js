20cbb9fadce75bf66d889ff466dc5449e0de0ef3
Stewding1990 <91507265+Stewding1990@users.noreply.github.com>
2024-08-20 10:32:05 +1000

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import bodyParser from "body-parser";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const API_URL = "https://pokeapi.co/api/v2/pokemon/";
 
 app.set("view engine", "ejs");
